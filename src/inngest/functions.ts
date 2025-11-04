727869013a4d37889319695e87849b7ef0b6dffd
taslim121 <taslim_ansari_comp@moderncoe.edu.in>
2025-11-04 23:10:59 +0530

    Ai-Providers Using AI Sdk

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,22 +1,51 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import {createGoogleGenerativeAI} from "@ai-sdk/google";
+import {createOpenAI} from "@ai-sdk/openai";
+import {createAnthropic} from "@ai-sdk/anthropic";
+import {generateText} from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+const googleAI = createGoogleGenerativeAI();
+const openAI = createOpenAI();
+const anthropicAI = createAnthropic();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    await step.sleep("retriving-Youtube-Video", "5s");
-    
-    await step.sleep("Transacribing-Youtube-Video", "5s");
+    const { steps:geminiSteps } = await step.ai.wrap(
+      "gemini-genrate-text",
+      generateText,
+      {
+        model: googleAI("gemini-2.0-flash"),
+        system : "You are a helpful assistant that helps people find information.",
+        prompt:"What is Semantic Layer Powered File Sytem in Short 50 words?"
+      }
 
-    await step.sleep("send-ai-to-summrize", "5s");
+    );
 
-    await step.run("create-workflow",()=>{
-      return prisma.workflow.create({
-        data: {
-          name: "Workflow from Inngest"
-        }
-      });
-    })
+    const { steps:openaiSteps } = await step.ai.wrap(
+      "openai-genrate-text",
+      generateText,
+      {
+        model: openAI("gpt-3.5-turbo"),
+        system : "You are a helpful assistant that helps people find information.",
+        prompt:"What is Semantic Layer Powered File Sytem in Short 50 words?"
+      }
+
+    );
+
+    const { steps:anthropicSteps } = await step.ai.wrap(
+      "anthropic-genrate-text",
+      generateText,
+      {
+        model: anthropicAI("claude-3-haiku-20240307"),
+        system : "You are a helpful assistant that helps people find information.",
+        prompt:"What is Semantic Layer Powered File Sytem in Short 50 words?"
+      }
+
+    );
+
+    return {geminiSteps,openaiSteps,anthropicSteps};
   },
 );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,6 +4,13 @@ import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async()=>{
+    await inngest.send({
+      name : "execute/ai",
+    });
+    return {success: true,message: "AI Job Queued"};
+  }),  
+  
   getWorkflows: protectedProcedure
     .query(({ctx}) => {
       return prisma.workflow.findMany();
