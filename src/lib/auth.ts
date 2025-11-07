b84637c68fc20d26767e6af3d4aa4d6af58a031f
taslim121 <taslim_ansari_comp@moderncoe.edu.in>
2025-11-07 22:28:28 +0530

    Paymnet Subscrption Addesd using Polar

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
 import { betterAuth } from "better-auth";
+import {checkout,polar,portal} from "@polar-sh/better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import { polarClient } from "./polar";
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma,{
     provider: "postgresql"
@@ -8,5 +11,26 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true
-  }
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use :[
+        checkout({
+            products:[
+              {
+                productId:"d5016dec-e360-43a2-aa7a-59826225dedb",
+                slug:"pro",
+
+              }
+            ],
+            successUrl: process.env.POLAR_SUCCESS_URL,
+            authenticatedUsersOnly: true,
+        }),
+        portal()
+      ],
+    })
+
+  ]
 });
